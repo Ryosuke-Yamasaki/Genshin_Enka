8eacea2ea529604c864dd3c24739585782ff3020
Ryosuke-Yamasaki <s2032144bu@s.chibakoudai.jp>
2023-10-01 13:57:20 +0900

    ejs added

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const app = express();
+const Router = require("./routes");
+
 const PORT = 8000;
+
+app.set("view engine", "ejs");
+
+app.use("/", Router);
 
 app.listen(PORT, () => console.log("サーバーが起動しました。"));
